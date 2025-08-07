2f6ef6b17fc735e57a3b78072a0b0c4633d3a72b
MJamil <mohammedfjameel@yahoo.com>
2025-08-07 15:38:14 +0200

    Updating branch name

--- LibraryPublishing.bat
+++ LibraryPublishing.bat
@@ -1,5 +1,5 @@
 
 call npm run build ^
 && call npm version patch -m "Release v%%s" ^
-&& git push origin VUE3_Branch --follow-tags ^
+&& git push origin dev --follow-tags ^
 && call npm publish --access public
